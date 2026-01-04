bf7d42964e13ea7b617195aa1df46056df8e3784
Mikhail Fedotov <mikhail.s.fedotov@expero.ru>
2026-01-05 01:31:43 +0700

    Init

---
+++ MultiThreadingProject/ArraySumTest.cs
@@ -0,0 +1,105 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace MultiThreadingProject;
+
+//[MinIterationCount(15)]
+//[MaxIterationCount(20)]
+//[InvocationCount(10)]
+[MemoryDiagnoser]
+[SimpleJob(RuntimeMoniker.Net90)]
+public class ArraySumTest
+{
+    private int[] sourceArray;
+
+    [Params(0)]
+    public int Min { get; set; }
+
+    [Params(100)]
+    public int Max { get; set; }
+
+    [Params(100000, 1000000, 10000000)]
+    public int Size { get; set; }
+
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        sourceArray = GenerateArrayOfInts(Min, Max, Size);
+    }
+
+    [Benchmark]
+    public void StandardSum()
+    {
+        var sum = 0;
+        foreach (var item in sourceArray)
+        {
+            sum += item;
+        }
+
+        Console.WriteLine("Sum of sequential process: {0}", sum);
+    }
+
+    [Benchmark]
+    public void ThreadSum()
+    {
+        //Разбиваем массив на куски.
+        //Возьмём кол-во кусков = кол-ву ядер процессора.
+        var partsCount = Environment.ProcessorCount;
+        var chunkSize = sourceArray.Length / partsCount + 1;
+        var chunks = sourceArray.Chunk(chunkSize)
+                                .ToArray();
+
+        var threads = new List<Thread>(chunks.Length);
+
+        var sum = 0;
+
+        // Каждый кусок обрабатываем в отдельном потоке.
+        foreach (var chunk in chunks)
+        {
+            var chunkThread = new Thread(() =>
+            {
+                var partSum = chunk.Sum();
+                Interlocked.Add(ref sum, partSum);
+            });
+
+            threads.Add(chunkThread);
+            chunkThread.Start();
+        }
+
+        // Потоки нужно синхронизировать и получить общий результат
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        Console.WriteLine("Sum of thread process: {0}", sum);
+    }
+
+    [Benchmark]
+    public void PlinkSum()
+    {
+        var sum = sourceArray.AsParallel()
+                             .AsOrdered()
+                             .Sum();
+
+        Console.WriteLine("Sum of plink process: {0}", sum);
+    }
+
+
+    /// <summary>
+    /// Метод генерит массив целых случайных чисел от min до max размером size.
+    /// </summary>
+    /// <param name="min">Минимальное число в массиве</param>
+    /// <param name="max">Максимальное число в массиве</param>
+    /// <param name="size">Размер массива</param>
+    /// <returns>Итоговый массив целых чисел.</returns>
+    static int[] GenerateArrayOfInts(int min, int max, int size)
+    {
+        var rand = new Random();
+
+        return Enumerable.Repeat(0, size)
+                         .Select(i => rand.Next(min, max))
+                         .ToArray();
+    }
+}
---
+++ MultiThreadingProject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MultiThreadingProject;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"Processor: {Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")}");
+        Console.WriteLine($"OS Description: {RuntimeInformation.OSDescription}");
+        Console.WriteLine($"OS Architecture: {RuntimeInformation.OSArchitecture}");
+        Console.WriteLine($"FrameworkDescription: {RuntimeInformation.FrameworkDescription}");
+
+
+        //BenchmarkRunner.Run<ArraySumTest>();
+
+        var test = new ArraySumTest
+        {
+            Min = 0,
+            Max = 100,
+            Size = 1000000
+        };
+
+        test.Setup();
+        test.StandardSum();
+        test.ThreadSum();
+        test.PlinkSum();
+    }
+}
